8d3fafa332f28c1f01743f00a4fbac65f6cd8d95
Rodrigo Longhi <rfslonghi@gmail.com>
2021-09-11 01:13:38 -0300

    Imagens ajustadas em tamanho e formato

--- srcs/start_game.c
+++ srcs/start_game.c
@@ -6,7 +6,7 @@
 /*   By: rfelipe- <rfelipe-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 23:59:05 by rfelipe-          #+#    #+#             */
-/*   Updated: 2021/09/10 00:56:57 by rfelipe-         ###   ########.fr       */
+/*   Updated: 2021/09/11 00:56:23 by rfelipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,15 @@
 void	start_game(t_game *game)
 {
 	game->mlx = mlx_init();
-	game->win.ref = mlx_new_window(game->mlx, 500, 500, "so_long");
+	game->win.ref = mlx_new_window(game->mlx, 600, 500, "so_long");
 	load_image(game);
+	for(int i = 0; i < 600 / 50; i++)
+	{
+		for(int j =0; j< 500 / 50; j++)
+		{
+			mlx_put_image_to_window(game->mlx, game->win.ref, game->empty.img, 50 * i, 50 * j);
+		}
+	}
 	mlx_put_image_to_window(game->mlx, game->win.ref, game->player.img, 0, 0);
 	mlx_put_image_to_window(game->mlx, game->win.ref, game->collect.img, 0, 50);
 	mlx_put_image_to_window(game->mlx, game->win.ref, game->wall.img, 0, 100);
